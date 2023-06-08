ecf7ace0bc67338f760a7ea2d98c2ced1e54fba1
laboratorio <laboratorio@UPOAULA10710>
2023-06-08 08:33:00 -0500

    Agregar archivos de proyecto.

---
+++ Pages/ConexionBD.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Pages
+{
+    public class ConexionBD
+    {
+        private static ConexionBD instance;
+        // private SqlConnection conectarbd;
+        private readonly SqlConnection con; 
+
+        private ConexionBD()
+        {
+            //string cadena = "Data Source=UPOAULA10710; Initial Catalog=DWQueue; Integrated Security=True";
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+        }
+
+
+        public static ConexionBD Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ConexionBD();
+                }
+                return instance;
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return con;
+        }
+
+        public void abrir()
+        {
+            try
+            {
+                con.Open();
+                Console.WriteLine("Conexion abierta");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al abrir la BD " + ex.Message);
+            };
+        }
+
+        public void cerrar()
+        {
+            con.Close();
+        }
+    }
+
+}
